2dc350b3eecd5aba29e8bdcd9e09600a8bd7e709
janek@projmen.pl <janek@projmen.pl>
2020-10-15 19:28:22 +0200

    Dodanie oceny

--- miejski.py
+++ miejski.py
@@ -1,6 +1,6 @@
+import discord
 import requests
 from bs4 import BeautifulSoup
-import discord
 import os
 
 client = discord.Client()
@@ -17,22 +17,32 @@ async def on_message(message):
         return
 
     if message.content.startswith('!miejski'):
-        url = 'https://www.miejski.pl/losuj'
-
-        r = requests.get(url, allow_redirects=True)
-        http = BeautifulSoup(r.text, 'html.parser')
-        redirected_url = http.find("link", {"rel": "canonical"})['href']
-        title = [x.get_text() for x in http.findAll("h1")]
-        definition = [x.get_text() for x in http.findAll("p")]
-        example = [x.get_text() for x in http.findAll("blockquote")]
-
-        if len(example) > 0:
-            response = "**Słowo:** " + title[0] + "\n**Definicja:** " + definition[0] + "\n**Przykład:**" \
-                       + example[0].replace('*', '\*') + "\n**URL**: " + redirected_url
-        else:
-            response = "**Słowo:** " + title[0] + "\n**Definicja:** " + definition[0] + "\n**URL**: " + redirected_url
+        response = await getMessage()
 
         await message.channel.send(response)
+
+
+async def getMessage():
+    url = 'https://www.miejski.pl/losuj'
+    r = requests.get(url, allow_redirects=True)
+    http = BeautifulSoup(r.text, 'html.parser')
+    redirected_url = http.find("link", {"rel": "canonical"})['href']
+    title = [x.get_text() for x in http.findAll("h1")]
+    definition = [x.get_text() for x in http.findAll("p")]
+    example = [x.get_text() for x in http.findAll("blockquote")]
+    rating = http.find("span", {"class": "rating"}).contents[0]
+    if len(example) > 0:
+        response = "**Słowo:** " + title[0] \
+                   + "\n**Ocena:** " + rating \
+                   + "\n**Definicja:** " + definition[0] \
+                   + "\n**Przykład:**" + example[0].replace('*', '\*') \
+                   + "\n**URL**: " + redirected_url
+    else:
+        response = "**Słowo:** " + title[0] \
+                   + "\n**Ocena:** " + rating \
+                   + "\n**Definicja:** " + definition[0] \
+                   + "\n**URL**: " + redirected_url
+    return response
 
 
 client.run(os.environ.get('DISCORD_TOKEN'))
