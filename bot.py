23d1a6bc377c9b8f92134e9ca8945b9a5482d2be
Chrrapek <marchr7@gmail.com>
2020-10-17 23:34:57 +0200

    reset hard + containing async function

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import asyncio
 import random
 import os
 import asyncpg
@@ -8,39 +9,38 @@ from miejski import Miejski
 from discord.ext import commands
 
 
-bot = commands.Bot(command_prefix='!')
-db = await asyncpg.create_pool(dsn=os.environ.get('DATABASE_URL'))
+async def run():
+    bot = commands.Bot(command_prefix='!')
+    db = await asyncpg.create_pool(dsn=os.environ.get('DATABASE_URL'))
 
+    @bot.event
+    async def on_ready():
+        print('Logged in as')
+        print(bot.user.name)
+        print(bot.user.id)
+        print('------')
+        await bot.get_channel(271732666653474826).send("Jestem od teraz w nowej wersji!")
 
-@bot.event
-async def on_ready():
-    print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
-    print('------')
-    await bot.get_channel(271732666653474826).send("Jestem od teraz w nowej wersji!")
+    @bot.command()
+    async def miejski(ctx: Context):
+        print('Recieved command !miejski from ' + ctx.author.name + ', processing...')
+        await db.execute(
+            'insert into users values(default, $1, $2, $3, 0) on conflict (server_id, user_id) do update set points = (select points from users where server_id=$1 and user_id=$2)+1;',
+            ctx.guild.id, ctx.author.id, ctx.author.name)
+        print('Executed database stuff')
+        await ctx.send(await Miejski.get_message())
 
+    @bot.command()
+    async def stats(ctx: Context):
+        print('Recieved stats request, processing...')
+        result = await db.fetch('select USER_NAME, POINTS from users where SERVER_ID=$1 order by POINTS desc;', ctx.guild.id)
+        await ctx.send(await Miejski.get_stats(result))
 
-@bot.command()
-async def miejski(ctx: Context):
-    print('Recieved command !miejski from ' + ctx.author.name + ', processing...')
-    await db.execute(
-        'insert into users values(default, $1, $2, $3, 0) on conflict (server_id, user_id) do update set points = (select points from users where server_id=$1 and user_id=$2)+1;',
-        ctx.guild.id, ctx.author.id, ctx.author.name)
-    print('Executed database stuff')
-    await ctx.send(await Miejski.get_message())
+    @bot.command()
+    async def choose(ctx, *choices: str):
+        await ctx.send(random.choice(choices))
 
+    bot.run(os.environ.get('DISCORD_TOKEN'))
 
-@bot.command()
-async def stats(ctx: Context):
-    print('Recieved stats request, processing...')
-    result = await db.fetch('select USER_NAME, POINTS from users where SERVER_ID=$1 order by POINTS desc;', ctx.guild.id)
-    await ctx.send(await Miejski.get_stats(result))
-
-
-@bot.command(description='Pomoc w decyzjach')
-async def choose(ctx, *choices: str):
-    await ctx.send(random.choice(choices))
-
-
-bot.run(os.environ.get('DISCORD_TOKEN'))
+loop = asyncio.get_event_loop()
+loop.run_until_complete(run())
