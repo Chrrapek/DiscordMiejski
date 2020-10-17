105644173bedbc18da26ca4f23a2a0f217f65262
Chrrapek <marchr7@gmail.com>
2020-10-17 17:15:02 +0200

    add util file, convert html tags into markdown used by Discord

---
+++ utils.py
@@ -0,0 +1,14 @@
+class Utils:
+    @staticmethod
+    def parse_html(text):
+        tag_dict = {
+            "<b>": "**",
+            "</b>": "**",
+            "<i>": "_",
+            "</i>": "_"
+        }
+        new_text = text.replace('*', '\*')
+        for i, j in tag_dict.items():
+            new_text = new_text.replace(i, j)
+        return new_text
+
