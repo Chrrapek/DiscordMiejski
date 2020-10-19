711e6c8c7ffea9cae077f4972cab6f47086dc6b9
Chrrapek <marchr7@gmail.com>
2020-10-19 20:50:32 +0200

    pls help me I am doing this too fast

--- cogs/HelpCog.py
+++ cogs/HelpCog.py
@@ -14,6 +14,6 @@ class HelpCog(commands.Cog):
                f'\n**!stats** - wyświetla liczbę punktów użytkowników serwera. Cooldown 30 sekund w ramach serwera' \
                f'\n**!gamble [wartość]** - 50% szans na podwojenie postawionej wartości, 50% na stracenie. Bez podania wartości do losowania idą wszystkie posiadane punkty użytkownika. Cooldown 10 minut dla użytkownika' \
                f'\n**!choose [opcje]** - losuje jedną z podanych opcji (opcje muszą być rozdzielone spacją)' \
-               f'\n**!help** - wyświetla tę pomoc. Cooldown 5 minut w ramach serwera'
+               f'\n**!pomoc** - wyświetla tę pomoc. Cooldown 5 minut w ramach serwera'
 
         await ctx.send(help_msg)
