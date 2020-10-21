dbaf4af6cb94f45038a4c144287b4449bff05751
janek@projmen.pl <janek@projmen.pl>
2020-10-21 23:02:26 +0200

    Parametr zabawy

--- cogs/HelpCog.py
+++ cogs/HelpCog.py
@@ -15,6 +15,6 @@ class HelpCog(commands.Cog):
                f'\n**!gamble [wartość]** - 50% szans na podwojenie postawionej wartości, 50% na stracenie. Bez podania wartości do losowania idą wszystkie posiadane punkty użytkownika. Cooldown 10 minut dla użytkownika' \
                f'\n**!choose [opcje]** - losuje jedną z podanych opcji (opcje muszą być rozdzielone spacją)' \
                f'\n**!pomoc** - wyświetla tę pomoc. Cooldown 5 minut w ramach serwera' \
-               f'\n**!zabawa** - Zgadywanie hasła po przykładzie'
+               f'\n**!zabawa** [czas w sekundach] - Zgadywanie hasła po przykładzie. Domyślna wartość czasu to 15s'
 
         await ctx.send(help_msg)
