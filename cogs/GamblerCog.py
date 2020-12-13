3daa68dc1b40cd6b316d971e93b4bbe3af5d1513
Danek <janek@projmen.pl>
2020-12-13 12:34:24 +0100

    Add test emoji

--- cogs/GamblerCog.py
+++ cogs/GamblerCog.py
@@ -5,7 +5,7 @@ from discord.ext.commands import Context
 
 from controllers.DatabaseController import DatabaseController
 from utils.ErrorMessages import ErrorMessages
-from utils.emoji import PEPE_SAD
+from utils.emoji import PEPE_SAD, PEPE_NAWROCKI
 
 
 class GamblerCog(commands.Cog):
@@ -29,7 +29,7 @@ class GamblerCog(commands.Cog):
             if amount == 0:
                 multiplier = random.choice([0, 2])
                 if multiplier == 0:
-                    await ctx.send(await self.lose_message(ctx))
+                    await ctx.send(self.va_banque_lose_message(ctx.author.name))
                 else:
                     await ctx.send(
                         f'Va banque! Brawo {ctx.author.name}, podwajasz swoje punkty i masz ich teraz {points * multiplier}!')
@@ -37,19 +37,26 @@ class GamblerCog(commands.Cog):
                 await self.db.upsert_user_points(f'{ctx.guild.id}', f'{ctx.author.id}', f'{ctx.author.name}', points)
             elif amount <= points:
                 if random.choice([0, 2]) == 2:
+                    new_points = points + amount
                     await self.db.upsert_user_points(f'{ctx.guild.id}', f'{ctx.author.id}', f'{ctx.author.name}',
-                                                     points + amount)
-                    await ctx.send(f'{ctx.author.name} wszedł pewniaczek i ma teraz {points + amount} punktów!')
+                                                     new_points)
+                    await ctx.send(self.win_message(ctx.author.name, new_points))
                 else:
+                    new_points = points - amount
                     await self.db.upsert_user_points(f'{ctx.guild.id}', f'{ctx.author.id}', f'{ctx.author.name}',
-                                                     points - amount)
-                    await ctx.send(f'{ctx.author.name} nie wszedł pewniaczek i ma teraz {points - amount} punktów...')
+                                                     new_points)
+                    await ctx.send(self.lose_message(ctx.author.name, new_points))
             elif amount > points:
                 await ctx.send(f'{ctx.author.name} nie cwaniakuj, nie masz tyle punkcików')
 
-    async def lose_message(self, ctx):
-        return f'Va banque! Niestety, {ctx.author.name}, ale tracisz wszystkie punkty...' + str(
-            self.bot.get_emoji(PEPE_SAD))
+    def va_banque_lose_message(self, name):
+        return f'Va banque! Niestety, {name}, ale tracisz wszystkie punkty...' + str(self.bot.get_emoji(PEPE_SAD))
+
+    def lose_message(self, name, new_points):
+        return f'{name} nie wszedł pewniaczek i ma teraz {new_points} punktów... ' + str(self.bot.get_emoji(PEPE_SAD))
+
+    def win_message(self, name, new_points):
+        return f'{name} wszedł pewniaczek i ma teraz {new_points} punktów! ' + str(self.bot.get_emoji(PEPE_NAWROCKI))
 
     @gamble.error
     async def gamble_error(self, ctx, error):
