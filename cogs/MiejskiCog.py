8f14a31a088f47f0b5152e78bf3f9e1f08c43182
Danek <janek@projmen.pl>
2021-04-12 19:25:02 +0200

    Test

--- cogs/MiejskiCog.py
+++ cogs/MiejskiCog.py
@@ -30,7 +30,7 @@ class MiejskiCog(commands.Cog):
     @miejski.error
     async def miejski_error(self, ctx, error):
         if isinstance(error, commands.CommandOnCooldown):
-            user = await self.bot.fetch_user(ctx.author.id)
+            user = self.bot.fetch_user(ctx.author.id)
             await user.send(f'Masz cooldown na !miejski. Jeszcze {error.retry_after} s')
         else:
             print(f'Error wywolany przez {ctx.author.name}: {error}')
